fd2be00e2d92c0049aad6e4a86a5b12384a9a506
florent-fauvin <florent-fauvin@users.noreply.github.com>
2015-05-21 12:32:46 +0200

    Update mywrapper.c

--- mywrapper.c
+++ mywrapper.c
@@ -1,5 +1,5 @@
 /*
- * Wrapper to fuzz the "myfunc" function in the "mylib" library
+ * Wrapper to fuzz the myfunc function defined in a library
  * Prototype of myfunc : int myfunc (int a, char* s);
  */
 
@@ -8,16 +8,17 @@
 
 void main(int argc, char **argv){
 
-        FILE* fp = fopen(argv[2],"r"); 	
-	/* mywrapper -f /path/input/input.txt */
-	/* FUZZING : /path/afl-fuzz -i /path/input -o /path/output/ -- /path/mywrapper -f @@ */
+        FILE* fp = fopen(argv[2],"r");
+	/* CALL : mywrapper -f /path/input/input.txt */
+	/* CALL BY AFL-FUZZ : /path/afl-fuzz -i /path/input -o /path/output/ -- /path/mywrapper -f @@ */
 
 	int a;
 	char* s; 
 	
 	// Reading "a" and "s" from input.txt
+	// ???
 
 	myfunc(a, s);
-	/* myfunc is WRAPPED */
-
+	/* myfunc is WRAPPED and CALLED */
+	
 }
